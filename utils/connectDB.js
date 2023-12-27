8c4da0f900b5e5281faebd07da9f066ae7dd0493
Mohamed Nabil <99597455+MohamedBarbary@users.noreply.github.com>
2023-12-27 02:20:52 +0200

    Add files via upload

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const catchAsyncErrors = require('./catchAsyncErrors');
 mongoose.set('strictQuery', false);
+dotenv.config();
 
 const connectDB = catchAsyncErrors(async (req, res, next) => {
   const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/social`);
