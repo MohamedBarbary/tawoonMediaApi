f73bc252374bc566b1dece6b1b15d0ef3d680214
Mohamed Nabil <99597455+MohamedBarbary@users.noreply.github.com>
2023-12-27 02:41:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const app = express();
 app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
 app.use(morgan('common'));
-app.use(
-  session({
-    secret: 'your_secret_key',
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+// app.use(
+//   session({
+//     secret: 'your_secret_key',
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/api/users', userRouter);
