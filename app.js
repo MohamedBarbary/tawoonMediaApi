ef6ccf787f21f53812e0eaded5b5b0c8a1a8aac2
Mohamed Nabil <99597455+MohamedBarbary@users.noreply.github.com>
2023-12-28 00:56:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const AppError = require('./utils/appError');
 const passport = require('./utils/authFeatures.js/googlePassport');
 const app = express();
 app.use(helmet());
+app.use(cors());
 app.use(express.json({ limit: '10kb' }));
 app.use(morgan('common'));
-app.use(cors());
 // app.use(
 //   session({
 //     secret: 'your_secret_key',
@@ -19,8 +19,8 @@ app.use(cors());
 //     saveUninitialized: true,
 //   })
 // );
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/api/users', userRouter);
 app.all('*', (req, res, next) => {
   //   const err = new Error(`can't find ${req.originalUrl} on server!`);
