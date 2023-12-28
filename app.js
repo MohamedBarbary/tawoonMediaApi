c995008a089da2082ef4650a5abc61e6cf039cf6
Mohamed Nabil <99597455+MohamedBarbary@users.noreply.github.com>
2023-12-28 02:37:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const userRouter = require('./routes/userRouter');
 const AppError = require('./utils/appError');
 const passport = require('./utils/authFeatures.js/googlePassport');
 const app = express();
-
+const cors = require('cors');
 // CORS middleware
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
 });
 
 app.use(helmet());
+app.use(cors());
 app.use(express.json({ limit: '10kb' }));
 app.use(morgan('common'));
 // Session setup
