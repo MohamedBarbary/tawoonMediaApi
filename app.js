c60dc8e87a4c6cc5d96c925c8938c7a5fa9acc82
Mohamed Nabil <99597455+MohamedBarbary@users.noreply.github.com>
2023-12-28 02:43:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 // CORS middleware
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
